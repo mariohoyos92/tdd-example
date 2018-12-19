5cc258282371b6ddc378d023202cdd689504287b
Mario Hoyos <mariohoyos92@gmail.com>
2018-12-19 09:20:34 -0600

    refactor: move server to own file so it can be used for tests

--- index.js
+++ index.js
@@ -1,8 +1,4 @@
-const express = require("express");
-const { json } = require("body-parser");
-
-const app = express();
-app.use(json());
+const app = require("./server");
 
 const port = 3005;
 app.listen(port, () => console.log(`Listening on: ${port}`));
---
+++ server.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const { json } = require("body-parser");
+
+const app = express();
+app.use(json());
+
+module.exports = app;
